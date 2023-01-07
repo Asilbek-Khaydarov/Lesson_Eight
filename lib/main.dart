3dfecd7f450afe5bb7afd69e95459653e29f0c1a
Asilbek <asilbek.goal.ftbl@gmail.com>
2023-01-07 15:36:18 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: const Text('Devider Example'),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: const Text(
+                  'JANUARY',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              IntrinsicHeight(
+                child: Row(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(top: 20),
+                          child: const Text(
+                            '1.2 h',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.w800),
+                          ),
+                        ),
+                        const Text('Friends per day'),
+                      ],
+                    ),
+                    const VerticalDivider(
+                      color: Colors.red,
+                      thickness: 2,
+                      width: 100,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(top: 20),
+                          child: const Text('5.0 h',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w800)),
+                        ),
+                          Container(
+                          child:  Text('Online per day'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
